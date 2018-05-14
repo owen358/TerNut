51f7f5cb1410f41c27dab4bc75cb86fe49d5c6e3
owen harrison <owenjharrison@protonmail.com>
2018-05-14 10:17:20 +0100

    commented out line 21 app/initializers/devise.rb due to deployment error heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'a01c4ab435931ed9b9423f2637c758c70f13a41c50f2ecb40362b40b1fde68c555656ebce276d4c5d6d2b25fabf4f61d6fa5d2e930d8f295ad4b1f4ab0ac6632'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
+
+#commented out lone below becasue app wouldnt deploy to heroku
+  #config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
